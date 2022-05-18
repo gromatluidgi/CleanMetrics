5df819cacfe005446f4b231a60961bfb0007b616
Luidgi Gromat <gromat.luidgi@gmail.com>
2022-05-18 23:39:27 +0200

    Initial commit

---
+++ CleanMetrics.cs
@@ -0,0 +1,27 @@
+﻿using CleanMetrics.Abstractions;
+using CleanMetrics.Metrics;
+using CleanMetrics.Metrics.Measures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMetrics
+{
+    public sealed class CleanMetrics
+    {
+        private readonly IMetricsContext localContext;
+        private readonly IMetricsContext remoteContext;
+
+        public CleanMetrics(IMetricsContext mainContext, IMetricsContext altContext)
+        {
+            localContext = mainContext;
+            remoteContext = altContext;
+
+            MeasureService = new MeasureService();
+        }
+
+        public MeasureService MeasureService { get; }
+    }
+}
---
+++ Metrics/Measures/Results/GetAllMeasuresResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMetrics.Metrics.Measures.Results
+{
+    internal class GetAllMeasuresResult
+    {
+    }
+}
